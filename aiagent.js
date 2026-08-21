1c6a7e18364cc28794d4679ef427c76b61384441
BluOm <bluomgroup@gmail.com>
2026-08-21 06:54:09 +0530

    Close POST AI sidebar overlay JSX

--- aiagent.js
+++ aiagent.js
@@ -1093,6 +1093,7 @@ function AIAgentScreen({ apiBaseUrl, userId = "anon", userName = "You", userAvat
               </button>
             </div>
           </div>
+        </div>
       )}
 
       {/* Library / Album — real files from /api/library, real upload/folder/delete */}
